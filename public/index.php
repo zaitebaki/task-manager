2527f966fd471f7fddf3481ae1b32cc9553d1579
ft17 <valery@churbanov74.ru>
2020-04-09 10:45:30 +0400

    Install npm packages

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+declare (strict_types = 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+echo "hello";
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const webpackHotConfig = require('./webpack.hot.config.js');
+const webpackDevConfig = require('./webpack.dev.config.js');
+const webpackBuildConfig = require('./webpack.build.config.js');
+
+const generalConfig = {
+  // абосолютный путь к папке с проектом
+  context: `${__dirname}/resourses`,
+
+  // при ошибке не собирать сборку
+  optimization: {
+    noEmitOnErrors: true,
+  },
+};
+
+module.exports = () => {
+  const currentMode = process.env.npm_lifecycle_event;
+  let webpackConfig;
+
+  // "горячая" сборка - Hot Module Replacement
+  if (currentMode === 'hot') {
+    webpackConfig = webpackHotConfig;
+  }
+
+  // итоговая сборка - удобочитаемый вариант
+  if (currentMode === 'dev') {
+    webpackConfig = webpackDevConfig;
+  }
+
+  // итоговая сборка - для загрузки на хостинг
+  if (currentMode === 'build') {
+    webpackConfig = webpackBuildConfig;
+  }
+
+  return _.merge(generalConfig, webpackConfig);
+};
