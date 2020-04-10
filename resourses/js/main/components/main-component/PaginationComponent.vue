014c6f7f5457e9b576b97f3ed841449226a77d39
ft17 <valery@churbanov74.ru>
2020-04-10 18:10:30 +0400

    Bumped version number to 0.2.1

--- resourses/js/main/components/main-component/PaginationComponent.vue
+++ resourses/js/main/components/main-component/PaginationComponent.vue
@@ -61,7 +61,7 @@ export default {
   },
   data() {
     return {
-      pageLink: "http://task-manager.devmasta.ru.com/page/"
+      pageLink: "http://manager.devmasta.ru.com/page/"
     };
   },
   computed: {
